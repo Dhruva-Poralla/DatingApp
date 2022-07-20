263c6120cadf4e0024a25a1c9a890a39f094ac8c
Dhruva-Poralla <poralladhruva@gmail.com>
2022-07-20 14:52:04 +0530

    End of section 4

---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
+import { AccountService } from '../_services/account.service';
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+
+  model:any={}
+  
+  
+  constructor(public accountservice:AccountService) { }
+
+  ngOnInit(): void {
+    
+  }
+
+Login()
+{
+  this.accountservice.login(this.model).subscribe(response=>{
+    console.log(response);
+    
+  },error=>{
+    console.log(error);
+  });
+  
+}
+ Logout(){
+   
+   this.accountservice.logout();
+ }
+
+ 
+
+}
