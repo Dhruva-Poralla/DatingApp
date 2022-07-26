9b2e2d019ff260a3102a8860c2d676d0d3a47cca
Dhruva-Poralla <poralladhruva@gmail.com>
2022-07-26 14:53:49 +0530

    End section8

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToastrModule } from 'ngx-toastr';
-import{BsDropdownModule} from 'ngx-bootstrap/dropdown'
+import{BsDropdownModule} from 'ngx-bootstrap/dropdown';
+import{TabsModule}from 'ngx-bootstrap/tabs';
+import {NgxGalleryModule} from '@kolkov/ngx-gallery'
 
 
 
@@ -12,12 +14,16 @@ import{BsDropdownModule} from 'ngx-bootstrap/dropdown'
     BsDropdownModule.forRoot(),
     ToastrModule.forRoot({
       positionClass:'toast-bottom-right'
-    })
+    }),
+    TabsModule.forRoot(),
+    NgxGalleryModule
 
   ],
   exports:[
     BsDropdownModule,
-    ToastrModule
+    ToastrModule,
+    TabsModule,
+    NgxGalleryModule
   ]
   
 })
